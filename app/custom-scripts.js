99e57433e70162be113abde33f0ee7a0a1ba01f2
Olivier <o_ribiere@cartong.org>
2019-11-27 21:46:57 +0100

    Change mobile style

--- app/custom-scripts.js
+++ app/custom-scripts.js
@@ -8,6 +8,10 @@ define(["dojo/topic"], function(topic) {
 		/*
 		* Custom Javascript to be executed when the application is ready goes here
 		*/
+		var d1 = document.getElementsByClassName('mobileTitle');
+		console.log(d1);
+		d1[0].innerHTML = '<img class="logoImg" src="https://www.arcgis.com/sharing/rest/content/items/9164cefd02f841aa969380dc6b282a10/resources/sm_series_logo__1571825220888.png" alt="" style="outline: none;"><div>UNAIDS Story Map</div>'
+
 	});
 
 	// Tab navigation.  This is fired anytime the tab is selected/changed.
